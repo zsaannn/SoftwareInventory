e6e0e5682ac9a66ed7b88688e9b365abb0b5d404
SergioGalindoCruz <100978144+SergioGalindoCruz@users.noreply.github.com>
2022-12-13 11:27:39 -0500

    Delete softwareinventory directory

--- softwareinventory/sistema/eliminar_productos.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!empty($_GET['id'])) {
-    require("../conexion.php");
-    $id = $_GET['id'];
-    $query_delete = mysqli_query($conectar, "DELETE FROM productos WHERE idproducto = $id");
-    mysqli_close($conectar);
-    header("location: listar_productos.php");
-}
-?>
--- softwareinventory/sistema/eliminar_usuarios.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!empty($_GET['id'])) {
-    require("../conexion.php");
-    $id = $_GET['id'];
-    $query_delete = mysqli_query($conectar, "DELETE FROM usuarios WHERE idusuario = $id");
-    mysqli_close($conectar);
-    header("location: listar_usuarios.php");
-}
-?>
